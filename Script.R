ab598902649233b87fcd4c2821afb2744202eea0
Oliver Gómez <90093706+olivergomezrs@users.noreply.github.com>
2024-05-09 15:23:04 -0600

    Add files via upload

--- Script.R
+++ Script.R
@@ -70,3 +70,40 @@ da <- ggplot(averageActivitydf, mapping = aes(Interval, Mean)) +
   theme_calc(base_family = "serif")
 
 print(da)
+
+
+#5 The 5-minute interval that, on average, contains the maximum number of steps
+averageDailyActivity[which.max(averageDailyActivity$Mean), ]$Interval
+
+#6 Code to describe and show a strategy for imputing missing data
+sum(is.na(activity$steps))
+
+# Matching the mean of daily activity with the missing values
+imputedSteps <- averageDailyActivity$Mean[match(activity$interval, averageDailyActivity$Interval)]
+
+# Transforming steps in activity if they were missing values with the filled values from above.
+activityImputed <- transform(activity, 
+                             steps = ifelse(is.na(activity$steps), yes = imputedSteps, no = activity$steps))
+
+# Forming the new dataset with the imputed missing values.
+totalActivityImputed <- aggregate(steps ~ date, activityImputed, sum)
+
+# Changing col names
+names(totalActivityImputed) <- c("date", "dailySteps")
+
+sum(is.na(totalActivityImputed$dailySteps))
+
+#7 Histogram of the total number of steps taken each day after missing values are imputed
+# Converting the data set into a data frame to be able to use ggplot2
+totalImputedStepsdf <- data.frame(totalActivityImputed)
+
+# Plotting a histogram using ggplot2
+p <- ggplot(totalImputedStepsdf, aes(x = dailySteps)) + 
+  geom_histogram(breaks = seq(0, 25000, by = 2500), fill = "#83CAFF", col = "black") + 
+  ylim(0, 30) + 
+  xlab("Total Steps Taken Per Day") + 
+  ylab("Frequency") + 
+  ggtitle("Total Number of Steps Taken on a Day") + 
+  theme_calc(base_family = "serif")
+
+print(p)
