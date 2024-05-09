cf077828fa5e07ff76ab6a8e5450e5f1106e9e32
Oliver Gómez <90093706+olivergomezrs@users.noreply.github.com>
2024-05-09 15:14:41 -0600

    Add files via upload

--- Script.R
+++ Script.R
@@ -1,3 +1,6 @@
+#1. Code for reading in the dataset and/or processing the data
+
+
 # Loading packages
 library(ggplot2)
 library(ggthemes)
@@ -19,3 +22,25 @@ activity <- cbind(activity, day)
 
 # Viewing the processed data
 summary(activity)
+
+
+#2 Histogram of the total number of steps taken each day
+
+# Calculating total steps taken on a day
+activityTotalSteps <- with(activity, aggregate(steps, by = list(date), sum, na.rm = TRUE))
+# Changing col names
+names(activityTotalSteps) <- c("Date", "Steps")
+
+# Converting the data set into a data frame to be able to use ggplot2
+totalStepsdf <- data.frame(activityTotalSteps)
+
+# Plotting a histogram using ggplot2
+g <- ggplot(totalStepsdf, aes(x = Steps)) + 
+  geom_histogram(breaks = seq(0, 25000, by = 2500), fill = "#83CAFF", col = "black") + 
+  ylim(0, 30) + 
+  xlab("Total Steps Taken Per Day") + 
+  ylab("Frequency") + 
+  ggtitle("Total Number of Steps Taken on a Day") + 
+  theme_calc(base_family = "serif")
+
+print(g)
